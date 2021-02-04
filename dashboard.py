d997c61200cd87c8d6a747205526b7a9f7ce5ff4
dudikbender <bender2242@gmail.com>
2021-02-04 21:14:04 +0000

    removed cytoscape and share details table

--- dashboard.py
+++ dashboard.py
@@ -65,20 +65,3 @@ wsb_posts.index +=1
 # r/wallstreetbets posts
 st.markdown('Top posts on [**r/wallstreetbets**](https://www.reddit.com/r/wallstreetbets/)')
 st.write(wsb_posts.to_html(escape=False), unsafe_allow_html=True)
-
-# Share Price table
-st.markdown(f'Share Price Details for **${algo.symbol}**')
-st.dataframe(prices_df.sort_index(ascending=False))
-
-cyto = go.Figure(cyto.Cytoscape(
-        id='cytoscape-two-nodes',
-        layout={'name': 'preset'},
-        style={'width': '100%', 'height': '400px'},
-        elements=[
-            {'data': {'id': 'one', 'label': 'Node 1'}, 'position': {'x': 75, 'y': 75}},
-            {'data': {'id': 'two', 'label': 'Node 2'}, 'position': {'x': 200, 'y': 200}},
-            {'data': {'source': 'one', 'target': 'two'}}
-        ]
-    )
-)
-st.plotly_chart(cyto)
